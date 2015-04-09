19c2b6fca52fd40bfbd237d5d52ce543079ac011
Vamsi Vidala <vamsi.vidala@forrent.com>
2015-04-09 12:21:54 -0400

    Adding initial resources

---
+++ README.md
@@ -0,0 +1,6 @@
+$ npm install http-server -g
+$ cd Angular101
+$ http-server -o --cors
+
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.3.15/angular.js"></script>
---
+++ snippets/js/singleListing.js
@@ -0,0 +1,39 @@
+{
+  "_id": 1000005,
+  "name": "Driftwood Apartments",
+  "floorplans": [{
+    "name": "The Coral",
+    "beds": 1,
+    "price": {
+      "low": 1115,
+      "high": 1260,
+      "formatted": "$1,115 - $1,260"
+    }
+  }, {
+    "name": "The Seashell",
+    "beds": 1,
+    "price": {
+      "low": 1115,
+      "high": 1210,
+      "formatted": "$1,115 - $1,210"
+    }
+  }],
+  "geo": {
+    "address_one": "800 W Grantline Rd",
+    "address_two": "",
+    "zip_code": "95376 ",
+    "city_name": "Tracy",
+    "state_code": "CA"
+  },
+  "main_image": {
+    "large_url": "http://thumbs.frmonline.com/imgs/fr/propertyFiles/005/000/1/resized/08_116439782508992371024076899001020.jpg"
+  },
+  "main_phone": {
+    "type": "Mobile",
+    "number": 3373990,
+    "area_code": 866,
+    "extension": null,
+    "description": "Mobile Provisioned Tracking Number",
+    "tracked": true
+  }
+}
